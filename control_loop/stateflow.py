bf5d225a5e2957442d27b0294c4a44d0e95a99b7
Pierre-Louis Veyrenc <pierrelouis.veyrenc@student-cs.fr>
2022-11-09 11:23:03 +0100

    Bugfixes

--- control_loop/stateflow.py
+++ control_loop/stateflow.py
@@ -10,14 +10,21 @@ detect_inter = 0
 detect_out = 0
 
 curr_state = SuivreLigne()
-instructions = ['A'] ################## A mieux initialiser
+
+def generator():
+    while True:
+        yield "F"
+
+
+instructions = generator() ################## A mieux initialiser
 
 def main():
+    global curr_state
     while True:
 
         erreur_orientation, detect_inter, detect_out = perception()
 
-        new_state = curr_state.transition(detect_obs = detect_obs, detect_inter = detect_out, detect_inter = detect_inter)
+        new_state = curr_state.transition(detect_obs = detect_obs, detect_out = detect_out, detect_inter = detect_inter)
         if new_state != curr_state:
             curr_state.exit()
         
