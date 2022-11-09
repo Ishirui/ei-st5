2168d56c69bf4b66e10b87f3dd1d55b8d1706781
Hugo L'Heureux <hugo.lheureux@student-cs.fr>
2022-11-09 14:19:52 +0100

    update de la comm avec arduino

--- premiers-tests-du-robot/robust_serial/envoi_commande_arduino.py
+++ premiers-tests-du-robot/robust_serial/envoi_commande_arduino.py
@@ -1,7 +1,7 @@
 # INITIALISATION DES FONCTIONS A IMPORTER UTILES
 
 
-from stateflow import fct_de_robin_et_pl  # //A MODIFIER\\
+# from stateflow import fct_de_robin_et_pl  # //A MODIFIER\\
 
 from robust_serial import write_order, Order, write_i8, write_i16, read_i16
 
@@ -10,19 +10,21 @@ from utils import open_serial_port
 serial_file = open_serial_port(baudrate=115200)
 
 
-v_max = 10  # m.s-1     //A MODIFIER\\
+v_max = 0.45  # m.s-1     //A MODIFIER\\
 
+R = 3
+r = 1
 ###################################################
 
 
-def envoi_commande():
-    (v, w) = fct_de_robin_et_pl()  # //A MODIFIER\\
+def envoi_commande(v, w):
+    # appel fonction stateflow  # //A MODIFIER\\
 
     # on va dire au moteurs qu'on les modifie
     write_order(serial_file, Order.MOTOR)
 
-    v_droite = v + w / 2
-    v_gauche = v - w / 2
+    v_droite = v + w
+    v_gauche = v - w
 
     # il faut remettre la valeur de la vitesse entre 0 et 100
     write_i8(serial_file, v_droite/v_max * 100)  # moteur droit
