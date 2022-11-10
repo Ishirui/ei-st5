e8f558ba94996ec70b05f1270bb26659a458361c
Arthur Baudin <arthur.baudin@student-cs.fr>
2022-11-10 10:39:49 +0100

    la premiere manoeuvre est chibree lol apres c bon

--- algos_chemins/routes.py
+++ algos_chemins/routes.py
@@ -17,16 +17,16 @@ def quadrillage(n):
 
     return g
 
-def deplacement_quadrillage(n, start, delivery_coords):
+def deplacement_quadrillage(n, start, delivery_coords, before_start):
     g = quadrillage(n)
-    d_c = [start[0]*n + start[1]] + [i[0]*n + i[1] for i in delivery_coords]
+    d_c = [start[1]*n + start[0]] + [i[1]*n + i[0] for i in delivery_coords]
     it_sorties, pos_list = itineraire(g, d_c)
     print(it_sorties, pos_list)
     manoeuvres = []
-    y = [i//n for i in pos_list]
-    x = [i%n for i in pos_list]
-    pos_counter = 1
-    for i in it_sorties[1:]:
+    y = [i//n for i in pos_list] + [before_start[1]]
+    x = [i%n for i in pos_list] + [before_start[0]]
+    pos_counter = 0
+    for i in it_sorties:
         print(manoeuvres)
         if i == -1:
             manoeuvres.append('livraison')
@@ -96,7 +96,7 @@ def deplacement_quadrillage(n, start, delivery_coords):
                             manoeuvres.append("milieu")
                             pos_counter += 1
             elif x[pos_counter] == 0:
-                    if y[pos_counter] < y[pos_counter - 1]:
+                    if y[pos_counter] > y[pos_counter - 1]:
                         if i == 1:
                             manoeuvres.append("milieu")
                             pos_counter += 1
@@ -138,7 +138,7 @@ def deplacement_quadrillage(n, start, delivery_coords):
     return(manoeuvres)
 
 
-print(deplacement_quadrillage(4, [0,0], [[3,3], [2,2], [0,3]]))
+print(deplacement_quadrillage(5, [1,0], [[3,3], [2,2], [0,3]], [0,0]))
 
 
                     
